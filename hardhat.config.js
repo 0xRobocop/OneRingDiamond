a4e110bd30022ddfd0ac28ecf7c598bf51b180fc
Alberto Rene <0xrobocop@protonmail.com>
2023-04-11 13:20:40 -0500

    third commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,12 +6,6 @@ module.exports = {
     optimism: {
       url: "https://mainnet.optimism.io",
     },
-
-    hardhat: {
-      forking: {
-        url: "https://opt-mainnet.g.alchemy.com/v2/n-z2yPh6ET1iUWbYOTO-UT3R5-cPCc9l",
-      }
-    }
   },
 
   solidity: {
